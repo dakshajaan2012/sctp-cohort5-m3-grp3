a127216c2fb000be5f450c76bc7edd4ae0378700
dakshajaan2012 <dakshajaan2012@gmail.com>
2024-03-18 20:47:31 +0800

    initial commit

---
+++ m3-p3/src/main/java/sg/edu/ntu/m3p3/M3P3Application.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.m3p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3P3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3P3Application.class, args);
+	}
+
+}
---
+++ m3-p3/src/test/java/sg/edu/ntu/m3p3/M3P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.m3p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3P3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
