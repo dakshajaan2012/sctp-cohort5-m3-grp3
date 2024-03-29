6109d2a6a15cd59eb04c8f24c624303e5101b0f9
dakshajaan2012 <dakshajaan2012@gmail.com>
2024-03-29 14:32:04 +0800

    Update ParkingSlot.java

--- m3-p3/src/main/java/sg/edu/ntu/m3p3/entity/ParkingSlot.java
+++ m3-p3/src/main/java/sg/edu/ntu/m3p3/entity/ParkingSlot.java
@@ -1,14 +1,20 @@
 package sg.edu.ntu.m3p3.entity;
 
+import java.util.UUID;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+//import sg.edu.ntu.m3p3.entity.User.User;
 
 @Data
 @Entity
@@ -19,20 +25,22 @@ import lombok.NoArgsConstructor;
 public class ParkingSlot {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Parking_id")
+    private UUID id;
 
+    @Column(name = "slot_number")
     private Integer slotNumber;
+
     @Column(nullable = false, columnDefinition = "boolean default false")
     @Builder.Default
     private Boolean isAvailable = true;
 
-    // Getters and Setters
-    public Long getId() {
+    public UUID getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(UUID id) {
         this.id = id;
     }
 
@@ -51,40 +59,5 @@ public class ParkingSlot {
     public void setIsAvailable(Boolean isAvailable) {
         this.isAvailable = isAvailable;
     }
-
-    /*
-     * public Long getId() {
-     * return id;
-     * 
-     * 
-     * public void setId(Long id) {
-     * this.id = id;
-     * }
-     * 
-     * public int getSlotNumber() {
-     * return slotNumber;
-     * }
-     * 
-     * public void setSlotNumber(int slotNumber) {
-     * this.slotNumber = slotNumber;
-     * }
-     * 
-     * public boolean isAvailable() {
-     * return isAvailable;
-     * }
-     * 
-     * public void setAvailable(boolean available) {
-     * isAvailable = available;
-     * }
-     */
-
-    /*
-     * // constructors
-     * public ParkingSlot(Long id, Integer slotNumber, Boolean isAvailable) {
-     * this.id = id;
-     * this.slotNumber = slotNumber;
-     * this.isAvailable = isAvailable;
-     * }
-     */
 
 }
