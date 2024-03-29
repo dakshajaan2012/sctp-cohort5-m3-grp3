a6f789fe00714de0d6d711889d3caca9597a7ed9
dakshajaan2012 <dakshajaan2012@gmail.com>
2024-03-29 14:34:40 +0800

    Update ParkingSlotService.java

--- m3-p3/src/main/java/sg/edu/ntu/m3p3/service/ParkingSlotService.java
+++ m3-p3/src/main/java/sg/edu/ntu/m3p3/service/ParkingSlotService.java
@@ -1,12 +1,28 @@
 package sg.edu.ntu.m3p3.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import sg.edu.ntu.m3p3.entity.ParkingSlot;
+import sg.edu.ntu.m3p3.entity.Session;
+//import sg.edu.ntu.m3p3.entity.User;
+import sg.edu.ntu.m3p3.entity.User.User;
+import sg.edu.ntu.m3p3.repository.ParkingSlotRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import sg.edu.ntu.m3p3.entity.ParkingSlot;
 import sg.edu.ntu.m3p3.repository.ParkingSlotRepository;
 
@@ -30,25 +46,7 @@ public class ParkingSlotService {
         return parkingSlotRepository.findAll();
     }
 
-    /*
-     * public void deleteSlots(Long id) {
-     * // Retrieve the parking slot by its ID
-     * Optional<ParkingSlot> optionalParkingSlot =
-     * parkingSlotRepository.findById(id);
-     * 
-     * // Check if the parking slot exists
-     * if (optionalParkingSlot.isPresent()) {
-     * // If the parking slot exists, delete it
-     * parkingSlotRepository.delete(optionalParkingSlot.get());
-     * } else {
-     * // If the parking slot does not exist, throw an exception or handle the error
-     * // accordingly
-     * throw new IllegalArgumentException("Parking slot not found with ID: " + id);
-     * }
-     * }
-     */
-
-    public void deleteSlots(Long id) {
+    public void deleteSlots(UUID id) {
 
         ParkingSlot parkingSlot = parkingSlotRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
