be9219defc0b17d6e3139979e3e2026acad8d5b8
dakshajaan2012 <dakshajaan2012@gmail.com>
2024-03-29 14:34:09 +0800

    Update SessionRepository.java

--- m3-p3/src/main/java/sg/edu/ntu/m3p3/repository/SessionRepository.java
+++ m3-p3/src/main/java/sg/edu/ntu/m3p3/repository/SessionRepository.java
@@ -1,22 +1,29 @@
 package sg.edu.ntu.m3p3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import sg.edu.ntu.m3p3.entity.Booking;
 import sg.edu.ntu.m3p3.entity.Session;
+//import sg.edu.ntu.m3p3.entity.User;
+//import sg.edu.ntu.m3p3.entity.User.User;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
+//import java.util.Optional;
 import java.util.UUID;
 
 public interface SessionRepository extends JpaRepository<Session, UUID> {
 
-    // @SuppressWarnings("null")
+    // Tested methods
     // Optional<Session> findById(UUID id);
+    // Optional<Session> findById(Long userId);
+    // List<Session> findByUserId(UUID userId);// was run
+    // Optional<Session> findById(Long userId);
 
-    Optional<Session> findById(UUID sessionId);
+    // Used custom method
 
-    void deleteByUser_UserId(UUID userId);
-
-    List<Session> findByUser_UserId(UUID userId);
+    @Query("SELECT b FROM Booking b WHERE b.user.userId = :userId")
+    List<Booking> findByUserId(@Param("userId") UUID userId);
 
 }
