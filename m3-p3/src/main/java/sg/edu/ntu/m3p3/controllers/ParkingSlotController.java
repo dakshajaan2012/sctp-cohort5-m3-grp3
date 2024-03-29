e3a3a5b0cfe0c65eaaeb368162ca32714419c23b
dakshajaan2012 <dakshajaan2012@gmail.com>
2024-03-29 14:31:01 +0800

    Update ParkingSlotController.java

--- m3-p3/src/main/java/sg/edu/ntu/m3p3/controllers/ParkingSlotController.java
+++ m3-p3/src/main/java/sg/edu/ntu/m3p3/controllers/ParkingSlotController.java
@@ -1,26 +1,26 @@
-package sg.edu.ntu.m3p3.controllers;
+package sg.edu.ntu.m3p3.Controllers;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import sg.edu.ntu.m3p3.entity.Booking;
+import sg.edu.ntu.m3p3.entity.ParkingSlot;
+import sg.edu.ntu.m3p3.entity.Session;
+//import sg.edu.ntu.m3p3.entity.User;
+import sg.edu.ntu.m3p3.entity.User.User;
+import sg.edu.ntu.m3p3.repository.BookingRepository;
+import sg.edu.ntu.m3p3.repository.ParkingSlotRepository;
+import sg.edu.ntu.m3p3.repository.UserRepository;
+import sg.edu.ntu.m3p3.service.ParkingSlotService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-import sg.edu.ntu.m3p3.entity.ParkingSlot;
-import sg.edu.ntu.m3p3.repository.ParkingSlotRepository;
-import sg.edu.ntu.m3p3.service.ParkingSlotService;
 
 @RestController
-@Tag(name = "Parking Slot", description = "Parking Slot APIs")
 @RequestMapping("/slots")
 public class ParkingSlotController {
     private static final Logger logger = LoggerFactory.getLogger(ParkingSlotController.class);
@@ -28,6 +28,8 @@ public class ParkingSlotController {
     @Autowired
     private ParkingSlotRepository parkingSlotRepository;
 
+    // Ok
+    // Endpoint - /slots
     @PostMapping
     public ResponseEntity<String> createParkingSlot(@RequestBody ParkingSlot parkingSlot) {
         if (parkingSlot.getSlotNumber() == null || parkingSlot.getSlotNumber() <= 0) {
@@ -35,7 +37,6 @@ public class ParkingSlotController {
         }
 
         // Save parking slot to the database
-        // parkingSlotRepository.save(parkingSlot);
 
         ParkingSlot savedParkingSlot = parkingSlotRepository.save(parkingSlot);
 
@@ -47,6 +48,8 @@ public class ParkingSlotController {
     @Autowired
     private ParkingSlotService parkingSlotService;
 
+    // Ok
+    // Endpoint /slots
     @GetMapping
     public ResponseEntity<List<ParkingSlot>> getAllSlots() {
         List<ParkingSlot> parkingSlots = parkingSlotService.getAllSlots();
@@ -55,15 +58,15 @@ public class ParkingSlotController {
             logger.error("GET /parking-slots called - No parking slots found");
             return ResponseEntity.notFound().build();
         }
-
         // Log info when parking slots are found
         logger.info("GET /parking-slots called - Found {} parking slots", parkingSlots.size());
 
         return ResponseEntity.ok(parkingSlots);
     }
 
+    // Ok
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteSlots(@PathVariable Long id) {
+    public ResponseEntity<String> deleteSlots(@PathVariable UUID id) {
         parkingSlotService.deleteSlots(id);
         logger.info("DELETE /slots/{} deleted", id);
         // return ResponseEntity.noContent().build();
